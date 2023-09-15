d88f0521644d92f7f6e183292c21e5e345cf2dfd
trishtrisha <131741926+trishtrisha@users.noreply.github.com>
2023-09-15 18:42:50 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,23 @@
+// JavaScript (script.js)
+
+let displayValue = "";
+
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById("display").value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = "";
+    document.getElementById("display").value = "";
+}
+
+function calculate() {
+    try {
+        displayValue = eval(displayValue);
+        document.getElementById("display").value = displayValue;
+    } catch (error) {
+        document.getElementById("display").value = "Error";
+        displayValue = "";
+    }
+}
